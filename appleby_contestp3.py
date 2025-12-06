1a3ae013d4f94d8fa126267fcbf3cb0452ac860a
ynst <yunust418@gmail.com>
2025-12-05 18:03:33 -0800

    solution added

--- appleby_contestp3.py
+++ appleby_contestp3.py
@@ -11,21 +11,40 @@ def calc(exp):
     calc("(+ 1 1)") -> calc("1") + calc("1") -> 1 + 1 -> 2
     
     '''
+    # print(exp)
+
     if exp in memory:
         return memory.get(exp)
 
-    if "(" not in exp:
+    if "(" not in exp: # exp must be just a number
         res = int(exp)
 
         memory[exp] = res
 
         return res
     
-    op = exp[1]
+    # exp = (+/- exp1 exp2)
+    op = exp[1] # + or -
     first = ""
     second = ""
 
-    ####### parsed the string and obtained the first and second
+    ####### parse the exp string and obtained the first and second exps
+    right_space = None
+    p, depth = 3, 0
+    while p < len(exp):
+        if exp[p] == "(":
+            depth += 1
+        elif exp[p] == ")":
+            depth -= 1
+
+        if exp[p] == " " and depth == 0:
+            right_space = p
+            break
+
+        p += 1
+
+    first = exp[3:right_space] # first expression
+    second = exp[right_space + 1:len(exp) - 1] # second expression
 
     res = 0
     if op == "+":
@@ -37,4 +56,6 @@ def calc(exp):
     
     return res
 
-    
+# print(calc("(+ 1 2)"))
+# print(calc("(+ 1 (+ (+ (+ 3 4) -2) 5))"))
+print(calc(input()))

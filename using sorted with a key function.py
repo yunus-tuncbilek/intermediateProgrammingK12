e4acaced12b664ee923c3b4ffbe9197ae40515b9
ynst <yunust418@gmail.com>
2025-12-12 18:01:34 -0800

    explanations

--- using sorted with a key function.py
+++ using sorted with a key function.py
@@ -4,3 +4,15 @@ def sorting_key(t):
 d = {1:3, 3:1 , 4:1}
 
 print(sorted(d.items(), key = sorting_key))
+
+# d.items() returns a list of (key, value) pairs in d. The pairs are tuples
+# d.keys() returns a list of the keys in d
+# d.values() returns a list of the values in d
+
+# A method is a part of a class. Anything that is called with a period is a method
+#   d.items()
+#   l.sort()
+# A function is more general and applies to any function but mostly we use
+#   this term for callables that are not methods
+#   len(l) 
+#   sorted(d.items)
